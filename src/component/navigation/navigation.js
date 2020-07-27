128a1be95f80a64c59ea436030fd209ba60df6b4
aifu <2661030790@qq.com>
2020-07-27 23:21:33 +0800

    画鸟

--- src/component/navigation/navigation.js
+++ src/component/navigation/navigation.js
@@ -1,47 +1,37 @@
 import React from 'react';
-
+import bird from './bird1.json';
 import './navigation.css';
 import { Link } from 'react-router-dom';
 
-function Navigation() {
-    const info1 = [{name: '麻雀', info: 'red'},
-    {name: '山雀', info: 'red'},
-    {name: '啄木鸟', info: 'red'}]
-    const listItems = info1.map((item) => 
-        <div className="nav-label" key={item.name}>
-            <div className="link">
-                <Link to={`/b?name=${item.name}`} >{item.name}</Link>
-            </div>
-
+const info1 = [{name: '麻雀', info: 'red'},
+{name: '山雀', info: 'red'},
+{name: '啄木鸟', info: 'red'}]
+const listItems = info1.map((item) => 
+    <div className="nav-label" key={item.name}>
+        <div className="link">
+            <Link to={`/b?name=${item.name}`} >{item.name}</Link>
         </div>
-    );
+
+    </div>
+);
+const polygonInfo = bird
+const polygonBird = 
+<ul className="bird">
+    {
+        polygonInfo.map((item) => 
+            <li className="range" style={item} key={item.key}></li>
+        )
+    }
+</ul>
+function Navigation() {
+
     return (
         <div className="nav">
             <div className="nav-contain">
                 <h2>这里可能出现...</h2>
+                {polygonBird}
                 {listItems}
-                <ul className="bird">
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                    <li className="range"></li>
-                </ul>
+                
             </div>
         </div>
     )
