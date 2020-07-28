c4e5d496a296cf3501134f43cbce90f3ed070066
aifu <2661030790@qq.com>
2020-07-29 00:04:20 +0800

    导航修改

--- src/component/navigation/navigation.js
+++ src/component/navigation/navigation.js
@@ -9,44 +9,63 @@ const info1 = [{name: '麻雀', info: 'red'},
 {name: '鹰', info: 'red'},
 {name: '海绵', info: 'red'},]
 const listItems = 
-<div className="nav-list">
-    {
-        info1.map((item) => 
+      info1.map((item) => 
             <div className="nav-label" key={item.name}>
                 <div className="link">
                     <Link to={`/b?name=${item.name}`} >{item.name}</Link>
                 </div>
             </div>
         )  
-    }
-</div>
-
-
-const polygonBird = 
-<ul className="bird"  onMouseEnter={birdNav}>
-    {
-        birdInfo.map((item) => 
+const polygonBird = birdInfo.map((item) => 
             <li className="range" style={item} key={item.key}></li>
         )
+
+
+
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            infoShow: 'none',
+            tips: '这里可能出现...'
+        }
+        this.reSearchShow = this.reSearchShow.bind(this)
+        this.reSearchHide = this.reSearchHide.bind(this)
     }
-</ul>
+    reSearchShow() {
+        this.setState({
+            tips: '换一批吗？',
+            infoShow: 'block'
+        })
+    }
+    reSearchHide() {
+        this.setState({
+            tips: '再看看吧',
+            infoShow: 'none'
+        })
+    }
+    render() {
+        return (
+            <div className="nav">
+                <div className="nav-contain">                 
+                    <div className="nav-list">
+                            {listItems}    
+                    </div>
 
-function birdNav(e) {
-    //polygonBird.props.children[3].props.style.backgroundColor = "#fff"
-}
-
-function Navigation() {
-
-    return (
-        <div className="nav">
-            <div className="nav-contain">
-                {polygonBird}
-                <h2>这里可能出现...</h2>
-                {listItems}
-                
+                    <div className="bird-contain">
+                        <div className="bird"  onMouseEnter={this.reSearchShow} onMouseLeave={this.reSearchHide}>
+                            <ul>
+                                {polygonBird}
+                            </ul>
+                            <h2 className="bird-tips">{this.state.tips}</h2>
+                            <div className="bird-button" style={{display: this.state.infoShow}}>换一批</div>
+                        </div>
+                    </div>                      
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
 }
 //import logo from './logo.svg'
             //<img src={logo} className="App-logo" alt="logo" />
