811ba1cb0f7ddffc68f725674acf91ebfa53861d
kancell <2661030790@qq.com>
2020-07-31 17:41:54 +0800

    样式修改

--- src/App-header.js
+++ src/App-header.js
@@ -18,9 +18,9 @@ class AppHeader extends React.Component {
                 <div className="header-contain">
                     <div className="header-left">
                         <div>       
-                            <div className="header-hover"><Link to="/">场景导航</Link></div>
-                            <div className="header-hover"><Link to="/b">瀑流图库</Link></div>
-                            <div className="header-hover"><Link to="/c">信息</Link></div>           
+                            <Link to="/"><div className="header-hover">场景导航</div></Link>
+                            <Link to="/b"><div className="header-hover">瀑流图库</div></Link>
+                            <Link to="/c"><div className="header-hover">信息</div></Link>      
                         </div>
                         <div className="header-search">
                             <input type = "text" placeholder="搜索感兴趣的内容..."/>
